172cbc9b4271708e5fb480552678a88ebeca8a22
KBENBENECK <kbenbeneck@gmail.com>
2021-06-01 10:27:50 -0400

    Move nav component to nav directory. Add basic css.

---
+++ src/components/nav/Nav.js
@@ -0,0 +1,14 @@
+import './Nav.css';
+import React from 'react';
+import logo from '../../imgs/logo.jpg'
+const Nav = () => {
+    return(
+        <div className="side-nav">
+            Nav
+            <img src={logo} alt="logo" className="nav-logo"></img>
+        </div>
+    );
+};
+export default Nav;
+//contact
+//social links
