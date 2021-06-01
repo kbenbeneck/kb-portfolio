34855b96d19de031a22dd20870c814818380f370
KBENBENECK <kbenbeneck@gmail.com>
2021-06-01 10:28:15 -0400

    Move landing to new diectory. Create basic css

---
+++ src/components/landing/Landing.js
@@ -0,0 +1,17 @@
+import './Landing.css';
+import React from 'react';
+const Landing = () => {
+    return(
+        <div className="landing">
+            <div className="greeting">
+                <p>
+                    Hi 
+                </p>
+            </div>
+        </div>
+    );
+};
+export default Landing;
+//cool background
+//greeting
+//button to main
