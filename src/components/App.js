331a91ad4da2f692e0289001943c99c564383c8e
KBENBENECK <kbenbeneck@gmail.com>
2021-06-01 10:29:37 -0400

    Create App.css. Include semantic ui grid styles.

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,9 @@
+import './App.css';
 import React from 'react';
 import About from './About';
 import Blog from './Blog';
-import Landing from './Landing';
+import Landing from './landing/Landing';
+import Nav from './nav/Nav';
 import Portfolio from './Portfolio';
 import Skills from './Skills';
 import Social from './Social';
@@ -9,13 +11,30 @@ import Social from './Social';
 
 const App = () => {
     return(
-        <div>
-            <Landing />
-            <About />
-            <Blog />
-            <Portfolio />
-            <Skills />
-            <Social />
+        <div className="app-bg">
+            <div className="ui grid">
+                <div className="ui row">
+                    <div className="sixteen wide column">
+                        <Landing />
+                    </div>
+                    <div className="three wide column">
+                        <Nav />
+                    </div>
+                    <div className="thirteen wide column">
+                        
+                        <About />
+                        <Blog />
+                        <Portfolio />
+                        <Skills />
+                        <Social />
+                    </div>
+
+                </div>
+
+            </div>
+            
+            
+            
         </div>
     );
 };
