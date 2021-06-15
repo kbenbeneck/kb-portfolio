e37cfe7bc03a4c1ad9fcb94eafcd4296381ff640
KBENBENECK <kbenbeneck@gmail.com>
2021-06-15 17:05:54 -0400

    Create reusable Cube component.

---
+++ src/components/cube/Cube.js
@@ -0,0 +1,21 @@
+import './Cube.css'
+import React from 'react'
+
+export const Cube = () => {
+    return (
+        <section className="cube">
+            
+
+                <div className="face left">
+                    Left
+                </div>
+                <div className="face top">
+                    Top
+                </div>
+                <div className="face right">
+                    Right
+                </div>
+            
+        </section>
+    )
+}
